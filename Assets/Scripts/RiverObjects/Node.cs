af61217a6a600e86c40d0cc7729f678d390a2177
Zibe <jean-baptiste.herve@3ie.fr>
2016-05-20 14:23:13 +0200

    First version of river nodes.

---
+++ Assets/Scripts/RiverObjects/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.RiverObjects
+{
+    class Node
+    {
+        public Vector3 position { get; set; }
+        public int priority { get; set; }
+        public int flow { get; set; }
+        public Node son { get; set; }
+
+        public Node(Vector3 position, int priority, int flow, Node son)
+        {
+            this.position = position;
+            this.priority = priority;
+            this.flow = flow;
+            this.son = son;
+        }
+
+        public bool isALeaf()
+        {
+            return son == null;
+        }
+    }
+}
